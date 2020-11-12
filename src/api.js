cb6b72d562b128634aace4309ebb5c621d6b948e
Henrique Thome <henrique.thome@hotmail.com>
2020-11-11 21:48:59 -0300

    f

--- src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: 'https://api.bitcointrade.com.br/v3/public/'
-});
-
-export default api;
--- src/apint.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const apint = axios.create({
-    baseURL: 'http://localhost:3003/'
-});
-
-export default apint;
